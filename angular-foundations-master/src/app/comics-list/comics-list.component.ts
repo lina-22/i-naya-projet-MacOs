6e4561e953a655bac868e2936ebe7641823f2d8e
mustfahinur <mustfahinur@gmail.com>
2023-11-20 20:17:26 +0100

    component comicsList init

---
+++ angular-foundations-master/src/app/comics-list/comics-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comics-list',
+  templateUrl: './comics-list.component.html',
+  styleUrls: ['./comics-list.component.css']
+})
+export class ComicsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
