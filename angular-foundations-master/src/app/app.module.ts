2375e1aeaa57510df44356f6569acf4f86975af4
mustfahinur <mustfahinur@gmail.com>
2023-11-20 18:55:44 +0100

    project angular init

---
+++ angular-foundations-master/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { TabBarComponent } from './tab-bar/tab-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ComicDetailComponent } from './comic-detail/comic-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    TabBarComponent,
+    NavBarComponent,
+    ComicDetailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
