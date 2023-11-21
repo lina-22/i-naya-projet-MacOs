e7278b4d52a0e8dd024ef918240c6138b8c801a4
mustfahinur <mustfahinur@gmail.com>
2023-11-21 15:24:12 +0100

    implimentation de routing

--- angular-foundations-master/src/app/routing.module.ts
+++ angular-foundations-master/src/app/routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ComicsListComponent } from './comics-list/comics-list.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, },
+  { path: 'comics', component: ComicsListComponent },
+  { path: 'about', component: AboutComponent }
+ 
+];
+
+// @NgModule({
+//   imports: [
+//     CommonModule
+//   ],
+//   declarations: []
+// })
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   declarations: []
 })
 export class RoutingModule { }
